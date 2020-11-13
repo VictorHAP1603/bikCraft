927251eec1ef410e65cf70e40a27426b0e5f1564
Victor Hugo <valvesperricci@yahoo.com.br>
2020-11-13 14:58:23 -0300

    feature de e-mail

--- js/main.js
+++ js/main.js
@@ -1,51 +1,51 @@
 // rolagem do menu
 
-function menuScroll() {
-    const sections = document.querySelectorAll('section')
-    const li = document.querySelectorAll('.header_menu ul li a')
-    const windowTop = window.pageYOffset + 92;
+// function menuScroll() {
+//     const sections = document.querySelectorAll('section')
+//     const li = document.querySelectorAll('.header_menu ul li a')
+//     const windowTop = window.pageYOffset + 92;
 
-    if (windowTop > sections[0].offsetTop) {
-        li[0].classList.add('active');
-    } else {
-        li[0].classList.remove('active');
-    }
+//     if (windowTop > sections[0].offsetTop) {
+//         li[0].classList.add('active');
+//     } else {
+//         li[0].classList.remove('active');
+//     }
         
 
-    if (windowTop > sections[1].offsetTop) {
-        for(let i = 0; i < li.length; i++) {
-            li[i].classList.remove('active')
-        }
-        li[1].classList.add('active')
-    } else {
-        li[1].classList.remove('active')
-    }
+//     if (windowTop > sections[1].offsetTop) {
+//         for(let i = 0; i < li.length; i++) {
+//             li[i].classList.remove('active')
+//         }
+//         li[1].classList.add('active')
+//     } else {
+//         li[1].classList.remove('active')
+//     }
 
 
-    if (windowTop > sections[2].offsetTop) {
-        for(let i = 0; i < li.length; i++) {
-            li[i].classList.remove('active')
-        }
-        li[2].classList.add('active')
-    } else {
-        li[2].classList.remove('active')
-    }
+//     if (windowTop > sections[2].offsetTop) {
+//         for(let i = 0; i < li.length; i++) {
+//             li[i].classList.remove('active')
+//         }
+//         li[2].classList.add('active')
+//     } else {
+//         li[2].classList.remove('active')
+//     }
 
 
-    if (windowTop > sections[3].offsetTop) {
-        for(let i = 0; i < li.length; i++) {
-            li[i].classList.remove('active')
-        }
-        li[3].classList.add('active')
-    } else {
-        li[3].classList.remove('active')
-    }
+//     if (windowTop > sections[3].offsetTop) {
+//         for(let i = 0; i < li.length; i++) {
+//             li[i].classList.remove('active')
+//         }
+//         li[3].classList.add('active')
+//     } else {
+//         li[3].classList.remove('active')
+//     }
 
-}
+// }
 
-menuScroll();
+// menuScroll();
 
-addEventListener('scroll', menuScroll)
+// addEventListener('scroll', menuScroll)
 
 // --------------------------------------------
 
@@ -109,4 +109,58 @@ Visibility.onVisible(function(){
     setTimeout(function () {
         $(".animar-interno").addClass("animated fadeInDown")
     }, 1200);
-})  
+});
+
+// -------------------------------------------------------
+
+$('.formphp').on('submit', function() {
+	var emailContato = "valvesperricci@yahoo.com.br"; // Escreva aqui o seu e-mail
+
+	var that = $(this),
+			url = that.attr('action'),
+			type = that.attr('method'),
+			data = {};
+	
+	that.find('[name]').each(function(index, value) {
+		var that = $(this),
+				name = that.attr('name'),
+				value = that.val();
+				
+		data[name] = value;
+	});
+	
+	$.ajax({
+		url: url,
+		type: type,
+		data: data,
+		success: function(response) {
+		
+			if( $('[name="leaveblank"]').val().length != 0 ) {
+				$('.formphp').html("<div id='form-erro'></div>");
+				$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-erro');
+				});
+			} else {
+			
+				$('.formphp').html("<div id='form-send'></div>");
+				$('#form-send').html("<span>Mensagem enviada!</span><p>Em breve eu entro em contato com você. Abraços.</p>")
+				.hide()
+				.fadeIn(1500, function() {
+				$('#form-send');
+				});
+			};
+		},
+		error: function(response) {
+			$('.formphp').html("<div id='form-erro'></div>");
+			$('#form-erro').html("<span>Falha no envio!</span><p>Você pode tentar novamente, ou enviar direto para o e-mail " + emailContato + " </p>")
+			.hide()
+			.fadeIn(1500, function() {
+			$('#form-erro');  
+		});
+		}
+	});
+	
+	return false;
+});
